605be503054fe7e5afcf1c6bad6313b77bcafabf
midgardppl <riof414@gmail.com>
2024-01-29 13:01:46 +0700

    admin pages

--- register.php
+++ register.php
@@ -1,21 +1,23 @@
 <?php
- $db = mysqli_connect('localhost','root','','yonseicoop');
- if(!$db)
- {
- echo "Database connection failed";
- }
- $username = $_POST['username'];
- $password = $_POST['password'];
- $sql = "INSERT INTO users VALUES ('".$username."', '".$password."')";
+$db = mysqli_connect('localhost','root','','yonseicoop');
+if(!$db)
+{
+	echo "Database connection failed";
+}
+$username = $_POST['username'];
+$password = $_POST['password'];
 
- $result = mysqli_query($db,$sql);
- $count = mysqli_num_rows($result);
-
- if($count == 1){
- 	echo json_encode("Success");
- } 
- else{
- 	echo json_encode("Error");
- }
-
+$sql = "SELECT username FROM users WHERE username = '".$username."'";
+$result = mysqli_query($db,$sql);
+$count = mysqli_num_rows($result);
+if($count == 1){
+	echo json_encode("Error");
+}else{
+	$insert = "INSERT INTO users (username, password) VALUES ('".$username."','".$password."')";
+		$query = mysqli_query($db,$insert);
+		if($query){
+			echo json_encode("Success");
+			// print $username;
+		}
+}
 ?>
